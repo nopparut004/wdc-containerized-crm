872ddba135b39b8f929610b0d43f1cf0b3b2076e
Worawan Diaz Carballo <papong@tu.ac.th>
2024-04-10 04:06:38 +0000

    Initial CRM project setup with security and Docker

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './src/routes/userRoutes.js';
+import jwt from 'jsonwebtoken';
+
+
+const app = express();
+const PORT = 3000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27044/CRMdb');
+
+// For parsing application/json
+app.use(express.json());
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+//JWT setup
+app.use((req, res, next) => {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+        jwt.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', (err, decode) => {
+            if (err) req.user = undefined;
+            req.user = decode;
+            next();
+        });
+    } else {
+        req.user = undefined;
+        next();
+    }
+});
+
+
+routes(app);
+
+app.get('/', (req, res) => 
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () => 
+    console.log(`Your server is running on port ${PORT}`)
+);
