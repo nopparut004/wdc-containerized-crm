f726c94f5d7ac54a79e8bd0590952f9167dfa0b6
Worawan Diaz Carballo <papong@tu.ac.th>
2024-04-11 15:37:58 +0700

    fix listening to localhost.

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.get('/', (req, res) =>
     res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
+app.listen(PORT, '0.0.0.0', () => 
     console.log(`Your server is running on port ${PORT}`)
 );
